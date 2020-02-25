a3ceb1b60a7a5e997b73d90b642f17b24ec3cf7e
hong-dev <jjuggumih@gmail.com>
2020-02-25 14:31:49 +0900

    setting.py에 secret key 설정 변경

--- zara/settings.py
+++ zara/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '06k8h6o^s!&w@*ayhf)c=8ooldug#ho0ht$@(7(wfj=_b-%r4x'
+SECRET_KEY = my_settings.SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
