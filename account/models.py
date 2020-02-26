9c13b4cbff300194dc520f139f19c020fb6bbd0e
hong-dev <jjuggumih@gmail.com>
2020-02-26 14:34:48 +0900

    create account app, account table, SignupView, SigninView

---
+++ account/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Account(models.Model):
+    email      = models.CharField(max_length = 50)
+    password   = models.CharField(max_length = 500)
+    name       = models.CharField(max_length = 30)
+    address    = models.CharField(max_length = 100)
+    country    = models.CharField(max_length = 30)
+    phone      = models.CharField(max_length = 15)
+    upper_body = models.CharField(max_length = 10, null = True)
+    lower_body = models.CharField(max_length = 10, null = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        db_table = 'accounts'
---
+++ account/views.py
@@ -0,0 +1,52 @@
+import json
+import bcrypt
+import jwt
+
+
+from zara.settings import SECRET_KEY
+from .models       import Account
+
+from django.views import View
+from django.http  import HttpResponse, JsonResponse
+
+class SignupView(View):
+    def post(self, request):
+        account_data = json.loads(request.body)
+
+        try:
+            if Account.objects.filter(email = account_data['email']).exists():
+                return HttpResponse(status = 409)
+
+            Account(
+                email    = account_data['email'],
+                password = bcrypt.hashpw(account_data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+                name     = account_data['name'],
+                address  = account_data['address'],
+                country  = account_data['country'],
+                phone    = account_data['phone'],
+            ).save()
+            return HttpResponse(status = 200)
+
+        except KeyError:
+            return JsonResponse({"message":"INVALID_KEYS"}, status = 400)
+
+
+class SigninView(View):
+    def post(self, request):
+        account_data = json.loads(request.body)
+
+        try:
+            if Account.objects.filter(email = account_data['email']).exists():
+                account = Account.objects.get(email = account_data['email'])
+
+                if bcrypt.checkpw(account_data['password'].encode('utf-8'), account.password.encode('utf-8')):
+                    token = jwt.encode({'email': account.email}, SECRET_KEY['secret'], algorithm = 'HS256').decode('utf-8')
+                    return JsonResponse({"token" : token}, status = 200)
+
+                return HttpResponse(status = 401)
+
+            return HttpResponse(status = 400)
+
+        except KeyError:
+            return JsonResponse({"message":"INVALID_KEYS"}, status = 400)
+
