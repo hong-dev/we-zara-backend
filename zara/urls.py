f065328266e0fdf7b263495e0eda2d88dd3c1336
hong-dev <jjuggumih@gmail.com>
2020-03-01 21:03:53 +0900

    clothes detail page API

--- zara/urls.py
+++ zara/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('account', include('account.urls')),
+    path('clothes', include('clothes.urls')),
+    path('store-info',include('store.urls'))
 ]
