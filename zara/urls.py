d13577665e909f7b2349c6d8e3b6eab3cb72a335
hong-dev <jjuggumih@gmail.com>
2020-03-06 23:36:04 +0900

    version merge

--- zara/urls.py
+++ zara/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('account', include('account.urls')),
-    path('store-info',include('store.urls')),
-    path('clothes', include('clothes.urls'))
+    path('store-info', include('store.urls')),
+    path('clothes', include('clothes.urls')),
+    path('orders', include('order.urls'))
 ]
